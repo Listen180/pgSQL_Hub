6540ff09109e2001516f965ff62ac350e6a039a3
listen <listen180@gmail.com>
2018-11-20 12:29:20 +0800

    make a new folder (a project)

---
+++ mall_area/calc_mall_area.py
@@ -0,0 +1,14 @@
+#!/home/leisen/anaconda3/bin/python3
+# -*- coding: UTF-8 -*-
+
+# ********************************************************
+# * Author        : LEI Sen
+# * Email         : sen.lei@outlook.com
+# * Create time   : 2018-11-20 12:28
+# * Last modified : 2018-11-20 12:28
+# * Filename      : calc_mall_area.py
+# * Description   : 
+# *********************************************************
+
+
+
